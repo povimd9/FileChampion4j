6ffcbd595ed575797905941fcd8a42eac2ad7157
ontghiwlkrg <mo.she100@gmail.com>
2023-03-26 16:58:14 -0400

    Added UT for ValidationResponse.class

---
+++ src/test/java/com/blumo/FileSentry4J/ValidationResponseTest.java
@@ -0,0 +1,36 @@
+package com.blumo.FileSentry4J;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+public class ValidationResponseTest {
+
+    @Test
+    public void testIsValid() {
+        ValidationResponse response = new ValidationResponse(true, null, null, null);
+        assertTrue(response.isValid());
+    }
+
+    @Test
+    public void testFailureReason() {
+        String expected = "file too large";
+        ValidationResponse response = new ValidationResponse(false, expected, null, null);
+        assertEquals(expected, response.failureReason());
+    }
+
+    @Test
+    public void testGetFileBytes() {
+        File expected = new File("test.txt");
+        ValidationResponse response = new ValidationResponse(false, null, expected, null);
+        assertEquals(expected, response.getFileBytes());
+    }
+
+    @Test
+    public void testGetFileChecksum() {
+        String expected = "abcdefg";
+        ValidationResponse response = new ValidationResponse(false, null, null, expected);
+        assertEquals(expected, response.getFileChecksum());
+    }
+}
