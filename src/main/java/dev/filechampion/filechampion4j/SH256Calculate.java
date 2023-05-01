2b49406025aedfda5519c20c0f9b3b31da2b912b
povimd9 <povimd9@users.noreply.github.com>
2023-05-01 17:00:04 -0400

    add condition for small files

--- src/main/java/dev/filechampion/filechampion4j/SH256Calculate.java
+++ src/main/java/dev/filechampion/filechampion4j/SH256Calculate.java
@@ -2,9 +2,12 @@ package dev.filechampion.filechampion4j;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -15,27 +18,36 @@ public class SH256Calculate {
     private static final int MIN_CHUNK_SIZE = 1024 * 1024; // 0.5 MB
     private static final int MAX_CHUNK_SIZE = 5 * 1024 * 1024; // 2 MB
     private final byte[] inputData;
+    private int byteSize;
 
-    public SH256Calculate(byte[] inputData) {
+    public SH256Calculate(byte[] inputData) throws IllegalArgumentException {
+        if (inputData == null || inputData.length == 0) {
+            throw new IllegalArgumentException("Input data must contain at least one byte.");
+        }
         this.inputData = inputData;
+        this.byteSize = inputData.length;
     }
 
-    public byte[] getChecksum() throws Exception {
-        return calculateSHA256Checksum();
+    public byte[] getChecksum() throws NoSuchAlgorithmException, InterruptedException, ExecutionException, IOException {
+        if (byteSize < MIN_CHUNK_SIZE * 2) {
+            return calculateSmallSHA256Checksum(inputData);
+        }
+        return calculateSHA256Checksum() ;
     }
 
-    private byte[] calculateSHA256Checksum() throws Exception {
+    private byte[] calculateSmallSHA256Checksum(byte[] fileBytes) throws NoSuchAlgorithmException {
+        return MessageDigest.getInstance("SHA-256").digest(fileBytes);
+    }
+
+    private byte[] calculateSHA256Checksum() throws NoSuchAlgorithmException, InterruptedException, ExecutionException, IOException  {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         ByteArrayInputStream bais = new ByteArrayInputStream(inputData);
-
         int numProcessors = Runtime.getRuntime().availableProcessors();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(numProcessors, numProcessors,
                 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-
         long fileSize = inputData.length;
         long chunkSize = Math.max(MIN_CHUNK_SIZE, Math.min(fileSize / numProcessors, MAX_CHUNK_SIZE));
         int numChunks = (int) Math.ceil((double) fileSize / chunkSize);
-
         byte[] buffer = new byte[(int) chunkSize];
         List<Future<?>> futures = new ArrayList<>(numChunks);
         for (int i = 0; i < numChunks; i++) {
@@ -43,12 +55,10 @@ public class SH256Calculate {
             Future<?> future = executor.submit(new ChecksumTask(md, buffer, bytesRead));
             futures.add(future);
         }
-
         // wait for all futures to complete
         for (Future<?> future : futures) {
             future.get();
         }
-
         executor.shutdown();
         bais.close();
         return md.digest();
